216fb5e364674fcfa5e052253629a61052a6f48d
Artyom <frolov.artyom.main@gmail.com>
2022-06-22 16:23:25 +0300

    Add an overload Equals, GetHashCode methods for IssueDetailsViewModel

--- BugTracker/Models/ViewModels/Issue/IssueDetailsViewModel.cs
+++ BugTracker/Models/ViewModels/Issue/IssueDetailsViewModel.cs
@@ -14,5 +14,35 @@ namespace BugTracker.Models.ViewModels.Issue
         public string? AssignedTo { get; set; }
         public string Priority { get; set; } = null!;
         public string Status { get; set; } = null!;
+
+        public override bool Equals(object? o)
+        {
+            if (ReferenceEquals(null, o)) return false;
+            if (ReferenceEquals(this, o)) return true;
+            return o.GetType() == GetType() && Equals((IssueDetailsViewModel) o);
+        }
+
+        protected bool Equals(IssueDetailsViewModel other)
+        {
+            return Id == other.Id && Title == other.Title && Description == other.Description &&
+                   Created.Equals(other.Created) && Nullable.Equals(Updated, other.Updated) &&
+                   CreatedBy == other.CreatedBy && AssignedTo == other.AssignedTo && Priority == other.Priority &&
+                   Status == other.Status;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = new HashCode();
+            hashCode.Add(Id);
+            hashCode.Add(Title);
+            hashCode.Add(Description);
+            hashCode.Add(Created);
+            hashCode.Add(Updated);
+            hashCode.Add(CreatedBy);
+            hashCode.Add(AssignedTo);
+            hashCode.Add(Priority);
+            hashCode.Add(Status);
+            return hashCode.ToHashCode();
+        }
     }
 }
